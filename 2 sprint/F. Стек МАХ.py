923f45a7ce770d1c9d98dfe0ca6ce76f0cec109c
ubuntu <v.litvinov2000@gmail.com>
2025-03-24 17:32:25 +0300

    com

---
+++ 2 sprint/F. Стек МАХ.py
@@ -0,0 +1,15 @@
+class Stack:
+    def __init__(self):
+        self.items = []
+
+def pop(self):
+    return self.items.pop()
+def push(self, item):
+    self.append(item)
+def get_max(self):
+    maximum = None
+    for i in self.items:
+        if i > maximum: maximum = i
+    return maximum
+
+

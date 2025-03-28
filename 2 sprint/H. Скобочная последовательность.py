73abaf2641e2c2679754892f87fff15d637c1e60
Vitaly_NB <v.litvinov2000@gmail.com>
2025-03-28 16:41:19 +0300

    com

--- 2 sprint/H. Скобочная последовательность.py
+++ 2 sprint/H. Скобочная последовательность.py
@@ -1,16 +1,24 @@
-def check(string):
-    cnt1, cnt2, cnt3 = 0, 0, 0
-    for i in string:
-        if i == '(': cnt1 += 1
-        elif i == ')': cnt1 -= 1
-        elif i == '[': cnt1 += 1
-        elif i == ']': cnt1 -= 1
-        elif i == '{': cnt1 += 1
-        elif i == '}': cnt1 -= 1
 
-        if cnt1<0 or cnt2<0 or cnt3<0:
-            return False
-    return True
+def check(string):
+    if string:
+        left = ''
+        for i in range(len(string)):
+            if string[i] in '([{':
+                if string[i] == '(': left += ')'
+                elif string[i] == '[': left += ']'
+                elif string[i] == '{': left += '}'
+            elif len(left) > 0 and left == string[i+len(left)-1:i-1:-1]:
+                left = ''
+                continue
+            elif len(left) > 0 and left != string[i+len(left)-1:i-1:-1]:
+                print(left + string[i+len(left)-1:i-1:-1])
+                return False
+        return True
+
+    else:
+        return True
 
 if __name__=="__main__":
     print(check(input()))
+
+#    {[()]}(()){{(([]))}}

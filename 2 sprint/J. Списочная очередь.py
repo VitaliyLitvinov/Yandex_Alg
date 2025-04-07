ad947a50a50e47435a1cfb4a63c23b0babbe35c8
Vitaly_NB <v.litvinov2000@gmail.com>
2025-04-07 17:41:35 +0300

    com

--- 2 sprint/J. Списочная очередь.py
+++ 2 sprint/J. Списочная очередь.py
@@ -1,63 +1,33 @@
 import sys
-class Linked_Deueu:
-    def __init__(self, head=None, value = None):
+class QueueNode:
+    def __init__(self, value: int, next=None) -> None:
         self.value = value
-        self.next = None
-        self.size = 0
+        self.next = next
 
-    def get(self, head, size):
-        if size != 0:
-            curr = head.value
-            head = head.next
-            sys.stdout.write(curr)
+class Queue:
+    def __init(self):
+        self.head = 0
+        self.tail = 0
+        self.size: int = 0
+    def put(self):
 
-        else: sys.stdout.write('error')
-        sys.stdout.write("\n")
+    def get(self):
 
-    def put(self, value, tail, head):
-        if tail:
-            tail.next = self
-            tail = self
-        else:
-            tail = self
-        if not head is None:
-            head = self
-        self.value = value
+    def size(self):
 
-def solution():
-    size = 0
-    n = int(sys.stdin.readline())
-    tail = None
-    head = None
-    for i in range(n):
-        request = sys.stdin.readline()
-        if request.startswith('put'):
-            node = Linked_Deueu()
-            node.put(request.rstrip().split()[-1], tail, head)
-            if not head is None:
-                head = node
-            size += 1
-            tail = node
-        if request.startswith('get', size):
-            node = Linked_Deueu()
-            node.get(head)
-            if not head is None:
-                node = Linked_Deueu()
-                head = node
-            if size != 0: size -= 1
-        if request.startswith('size'):
-            sys.stdout.write(str(size))
-            sys.stdout.write("\n")
-if __name__=="__main__":
-    solution()
-# 10
-# put -34
-# put -23
-# get
-# size
-# get
-# size
-# get
-# get
-# put 80
-# size
+
+
+def solution(n):
+    queue: Queue = Queue()
+    for _ in range(n):
+        request: str = sys.stdin.readline().strip()
+        if request.startswith('get'):
+            queue.get()
+        elif request.startswith('put'):
+            queue.put()
+        elif request.startswith("size"):
+            queue.size()
+
+
+if __name__ == "__main__":
+    solution(sys.stdin.readline().strip())
--- sprint_2/B. Список дел.py
+++ sprint_2/B. Список дел.py
@@ -1,18 +1,63 @@
-class Node:
-    def __init__(self, value=None, next=None):
+import sys
+class Linked_Deueu:
+    def __init__(self, head=None, value = None):
         self.value = value
-        self.next = next
-def solution(node):
-    while node:
-        print(node.value, end='->')
-        node = node.next
-    print('None')
+        self.next = None
+        self.size = 0
 
-def test():
-    n3 = Node('3')
-    n2 = Node('2', n3)
-    n1 = Node('1', n2)
-    solution(n1)
+    def get(self, head, size):
+        if size != 0:
+            curr = head.value
+            head = head.next
+            sys.stdout.write(curr)
 
+        else: sys.stdout.write('error')
+        sys.stdout.write("\n")
+
+    def put(self, value, tail, head):
+        if tail:
+            tail.next = self
+            atail = self
+        else:
+            tail = self
+        if not head is None:
+            head = self
+        self.value = value
+
+def solution():
+    size = 0
+    n = int(sys.stdin.readline())
+    tail = None
+    head = None
+    for i in range(n):
+        request = sys.stdin.readline()
+        if request.startswith('put'):
+            node = Linked_Deueu()
+            node.put(request.rstrip().split()[-1], tail, head)
+            if not head is None:
+                head = node
+            size += 1
+            tail = node
+        if request.startswith('get', size):
+            node = Linked_Deueu()
+            node.get(head)
+            if not head is None:
+                node = Linked_Deueu()
+                head = node
+            if size != 0: size -= 1
+        if request.startswith('size'):
+            sys.stdout.write(str(size))
+            sys.stdout.write("\n")
 if __name__=="__main__":
-    test()
+    solution()
+# 10
+# put -34
+# put -23
+# get
+# size
+# get
+# size
+# get
+# get
+# put 80
+# size

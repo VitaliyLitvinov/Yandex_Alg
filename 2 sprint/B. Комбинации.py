3bef0ee5f4cb5500d44f5a705b5a711354ca88cc
Vitaly_NB <v.litvinov2000@gmail.com>
2025-04-11 14:19:28 +0300

    com

--- 2 sprint/B. Комбинации.py
+++ 2 sprint/B. Комбинации.py
@@ -1,9 +1,13 @@
 import sys
-def combination(numbers: str, n: int) -> str:
+def combination(numbers: str) -> str:
     letters = {2:'abc', 3:'def', 4:'ghi', 5:'jki', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
-    
+    if len(numbers) <= 1:
+        return letters[int(numbers[0])]
+#    return letters[int(numbers[0])] + combination(numbers[1:], n)
+    for i in letters[int(numbers[0])]:
+        return i + combination(numbers[1:])
 
 
 if __name__ == "__main__":
     nums = sys.stdin.readline().rstrip()
-    combination(nums, len(nums))
+    print(combination(nums))
